36e24058ab839efaf26c4e90d9da8a97342f449d
JPCOMPAN <joaosilvapinheiro1310@gmail.com>
2025-05-26 19:57:55 -0300

    Update server.js

--- Codigo/server.js
+++ Codigo/server.js
@@ -3,11 +3,13 @@ const app = express();
 
 app.use(express.json());
 
-let items = [
+let filaAtendimento = [
     { id: 1, nome: "João Pablo"}
 ];
 
-
+app.get('/items', (req, res) => {
+    res.json(filaAtendimento);
+});
 
 const PORT = 3000;
 app.listen(PORT, () => console.log('Servidor rodando em http://localhost:${PORT}'));
